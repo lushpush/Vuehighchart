fdd3fa77597f66886444f04a31d6b7172040828b
STPCCI <149810788+STPCCI@users.noreply.github.com>
2025-05-21 16:47:59 -0500

    Fix database path resolution in backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors');
+const path = require("path");
 
 const app = express();
-const db = new sqlite3.Database('./mydatabase.db');
+const dbPath = path.join(__dirname, "mydatabase.db");
+const db = new sqlite3.Database(dbPath);
 
 app.use(cors());
 
