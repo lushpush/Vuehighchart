2c174ccf50f54a61b69a433e4e4e8a84baa4c902
lushpush <shaikh_ferhan@yahoo.com>
2025-05-23 00:13:55 -0500

    Update nuxt.config.ts

--- frontend/nuxt-highcharts-treemap/nuxt.config.ts
+++ frontend/nuxt-highcharts-treemap/nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  components: true,
+  build: {
+    transpile: ['@highcharts/vue']
+  }
   devtools: { enabled: true }
 })
+
